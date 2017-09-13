1a0c3cc63d07f4a4fd9386880746fe88baf6bfc1
147y1a0523 <31914913+147y1a0523@users.noreply.github.com>
2017-09-13 14:37:58 +0530

    Add files via upload

--- StudentGroup.java
+++ StudentGroup.java
@@ -11,6 +11,7 @@ import java.util.Date;
  * DO NOT PUT any classes into packages
  *
  */
+private ArrayList<Student> students;
 public class StudentGroup implements StudentArrayOperation {
 
 	private Student[] students;
@@ -19,121 +20,141 @@ public class StudentGroup implements StudentArrayOperation {
 	 * DO NOT remove or change this constructor, it will be used during task check
 	 * @param length
 	 */
-	public StudentGroup(int length) {
-		this.students = new Student[length];
-	}
+	
+public StudentGroup(int length)
+ {
+	return students.size();
+	this.students = new Student[length];
+}
+
+@Override
+public Student[] getStudents() 
+{
+	return Arrays.copy(students,student.length);
+	return null;
+}
+
+@Override
+public void setStudents(Student[] students)
+ {
+	return Arrays.copy(students,student.length);
+	return null;
+}
+
+@Override
+public Student getStudent(int index) 
+{
+	return Arrays.copy(students,student.length);
+	return getStudent().getstudent() +"" int index";
+	return null;
+}
+
+@Override
+public Student setStudent(int index) 
+{
+	for(int i=0;i<student.length;i++)
+		{
+			if(student==student[i])
+			continue;
+			if(student[i]==null)
+			student[i]=student;
+			return;
+		}
+}
+public void add(Student student, int index) {
+for(int i=0;i<student.length;i++)
+{
+	if(student==student[i])
+	continue;
+	if(student[i]==null)
+	student[i]=student;
+	return;
+}
+@Override
+public void remove(int index)
+ {
+	arrlist.remove();
+	 System.out.println("Now, Size of list: " + arrlist.size());
+}
+
+@Override
+public void remove(Student student) 
+{
+	students.remove(student);
+}
+public void removeFromElement(Student student) 
+{
+students.removeelement(student);
+}
+ 
+public static String remove(String studentID) 
+{
+        for(Student s : studentList) 
+{
+            String ident = s.getStudentID();
+        s.remove("3");                               
+        s.remove("3");                                      
+        if (!ident.equals(s))
+            System.out.println(ident + " does not exist.");
+        }
+    }
+}
 
 	@Override
-	public Student[] getStudents() {
-		// Add your implementation here
-		return null;
-	}
-
-	@Override
-	public void setStudents(Student[] students) {
-		// Add your implementation here
-	}
-
-	@Override
-	public Student getStudent(int index) {
-		// Add your implementation here
-		return null;
-	}
-
-	@Override
-	public void setStudent(Student student, int index) {
-		// Add your implementation here
-	}
-
-	@Override
-	public void addFirst(Student student) {
-		// Add your implementation here
-	}
-
-	@Override
-	public void addLast(Student student) {
-		// Add your implementation here
-	}
-
-	@Override
-	public void add(Student student, int index) {
-		// Add your implementation here
-	}
-
-	@Override
-	public void remove(int index) {
-		// Add your implementation here
-	}
-
-	@Override
-	public void remove(Student student) {
-		// Add your implementation here
-	}
-
-	@Override
-	public void removeFromIndex(int index) {
-		// Add your implementation here
-	}
-
-	@Override
-	public void removeFromElement(Student student) {
-		// Add your implementation here
-	}
-
-	@Override
-	public void removeToIndex(int index) {
-		// Add your implementation here
+	public void removeToIndex(int index) 
+	{
+	students.remove(i);
 	}
 
 	@Override
 	public void removeToElement(Student student) {
-		// Add your implementation here
+		students.removeelement(student);
 	}
 
-	@Override
-	public void bubbleSort() {
-		// Add your implementation here
-	}
+@Override
+public void bubbleSort() 
+{
+	for (int i = 0; i < numbers.length; i++){
+     System.out.println(numbers[i].toString());
+}
 
 	@Override
 	public Student[] getByBirthDate(Date date) {
-		// Add your implementation here
+		this.birthDate = birthDate;
 		return null;
 	}
 
 	@Override
 	public Student[] getBetweenBirthDates(Date firstDate, Date lastDate) {
-		// Add your implementation here
+		student date of birth in "yyyy-MM-dd"=this.return birthDate;
 		return null;
 	}
 
-	@Override
-	public Student[] getNearBirthDate(Date date, int days) {
-		// Add your implementation here
-		return null;
-	}
+	
 
 	@Override
 	public int getCurrentAgeByDate(int indexOfStudent) {
-		// Add your implementation here
+		this.ByDate=indexOFStudent;		
 		return 0;
 	}
 
 	@Override
 	public Student[] getStudentsByAge(int age) {
-		// Add your implementation here
+		this.ByAge = Byage;
 		return null;
 	}
 
 	@Override
-	public Student[] getStudentsWithMaxAvgMark() {
-		// Add your implementation here
+	public Student[] getStudentsWithMaxAvgMark()
+ {
+		this.student=this.avgmarks
 		return null;
 	}
 
 	@Override
 	public Student getNextStudent(Student student) {
-		// Add your implementation here
+		student s=new student;
+		s.nextstudent();
 		return null;
 	}
 }
